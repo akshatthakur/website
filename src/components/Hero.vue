790d8c061425687a701e0611a1d7be8d16a71b45
Akshat Thakur <79204593+akshatthakur@users.noreply.github.com>
2022-03-19 15:38:59 -0700

    Update Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -7,7 +7,7 @@
         <hr>
         <paragraph>I've always had a deep passion for creating new things and solving problems with no pre-existing solutions using technology and innovation. Essentially, I want to use a combination of statistics, math, and computer science to create cool stuff.</paragraph>
         <hr>
-        <paragraph>Following my recent graduation from the University of British Columbia with a BSc. in Statistics and Computer Science, I'm working at a Series-B funded Vancouver tech startup, where I spend all day flirting with big data and competitive intelligence. If you need me for anything data-sciencey, or want to chat about jazz and rock 'n' rock, Taylor Swift, writing, or anything else over a cup of coffee, feel free to reach out to me <linked href="mailto:akshatvsthakur@gmail.com">here</linked>.</paragraph>
+        <paragraph>Following my recent graduation from the University of British Columbia with a <u>BSc. in Statistics and Computer Science</u>, I'm working at a Series-B funded Vancouver tech startup, where I swim in big data and competitive intelligence. If you need me for anything data-sciencey, or want to chat about jazz and rock 'n' rock music, writing, or anything else, chat with me <linked href="mailto:akshatvsthakur@gmail.com">here</linked>.</paragraph>
 
         <social-links>
           <social-link-text><linked href="https://www.linkedin.com/in/akshatthakur" target="_blank"><i class="fab fa-linkedin"></i> LinkedIn</linked></social-link-text>
